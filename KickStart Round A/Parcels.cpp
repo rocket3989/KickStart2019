ccbd3f7374d89b54e82f5dcd62cb4f1b63252637
Collin <rocket3989@gmail.com>
2019-03-24 09:02:41 -0500

    added manhattan eval for max nodes

--- KickStart Round A/Parcels.cpp
+++ KickStart Round A/Parcels.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <cstdlib>
 
 using namespace std;
 
@@ -55,12 +57,12 @@ int main(){
     int tests = 0;
     cin >> tests;
     for(int test = 0; test < tests; test++){
-        int R, C, max = 0, x = 0, y = 0;
+        int R, C, max = 0;
         char input;
         
         cin >> R >> C;
 
-        for(int r = 0; r < R; r++){
+        for(int r = 0; r < R; r++){         //read input
             for(int c = 0; c < C; c++){
                 cin >> input;
                 if(input == '0')
@@ -68,62 +70,107 @@ int main(){
                 else
                     net[r][c] = 0;
             }
-        } //read input
+        } 
 
-        //printNet(R,C);
 
-        evalNet(R,C);
+        evalNet(R,C);       //build the network
 
         printNet(R,C);
 
-        int maxadj = 0;
+        vector <int> row;
+        vector <int> col;
+
+
 
         for(int r = 0; r < R; r++){
             for(int c = 0; c < C; c++){
-                if(max < net[r][c]){        //find where biggest is
+                if(max < net[r][c]){        //find what the biggest is
                     max = net[r][c];
-                    maxadj = -1;
+                    row.clear();
+                    col.clear();
                 }
                 
                 if (max == net[r][c]){
-                    int adj = 0;
-                    if(r > 0 && net[r-1][c] == max)
-                        adj++;
-                    
-                    if(r < R - 1 && net[r+1][c] == max)
-                        adj++;
-                
-                    if(c > 0 && net[r][c-1] == max)
-                        adj++;
-                    
-                    if(c < C - 1 && net[r][c+1] == max)
-                        adj++;
-                    
-                    if(adj > maxadj){
-                        x = r;
-                        y = c;
-                        maxadj = adj;
-                    }
+                    row.push_back(r);       //add each instance of biggest to ordered pair
+                    col.push_back(c);
                 }
             }
         }
-        cout << maxadj << '\n' << '\n';
-
-        net[x][y] = 0;
-
-        //cout << "X is: " << x << " Y is: " << y << '\n';
         
-        evalNet(R,C);
+        bool found = false;
+        int best;
+        for(int i = 0; i < row.size() && !found; i++){
+            bool inRange = true;
+            for(int j = 0; j < row.size() && inRange; j++){
+                if(abs(row[i]-row[j]) + abs(col[i] - col[j]) > max - 1)
+                    inRange = false;
+            }
+            if(inRange){
+                best = i;
+                found  = true;
+            }
+        }
 
-        printNet(R,C);
-        
-        max = 0;
-        for(int r = 0; r < R; r++)
-            for(int c = 0; c < C; c++)
-                if(max < net[r][c])
-                    max = net[r][c];
+        if(found){
+            net[row[best]][col[best]] = 0;
+
+            evalNet(R,C);
+
+            printNet(R,C);
+            
+            max = 0;
+            for(int r = 0; r < R; r++)
+                for(int c = 0; c < C; c++)
+                    if(max < net[r][c])
+                        max = net[r][c];
+
+        }
 
         cout << "Case #" << test + 1 << ": "<< max << endl;
         
     }
 }
+
+/*
+6
+3 3
+101
+000
+101
+1 2
+11
+5 5
+10001
+00000
+00000
+00000
+10001
+10 10
+1000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000000
+0000000001
+5 7
+1110111
+1100011
+1000001
+1100011
+1111111
+9 5
+11101
+11000
+10000
+00000
+00000
+00000
+00001
+00011
+10111
+
+*/
